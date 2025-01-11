c7b313e472052c222b551e54178e1386ef600b83
ikkkkeee <2212479414@qq.com>
2025-01-11 10:42:18 +0800

    first commit

---
+++ program_one/pages/index/index.js
@@ -0,0 +1,42 @@
+Page({
+  onLoad(query) {
+    // 页面加载
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  },
+  onReady() {
+    // 页面加载完成
+  },
+  onShow() {
+    // 页面显示
+  },
+  onHide() {
+    // 页面隐藏
+  },
+  onUnload() {
+    // 页面被关闭
+  },
+  onTitleClick() {
+    // 标题被点击
+  },
+  onPullDownRefresh() {
+    // 页面被下拉
+  },
+  onReachBottom() {
+    // 页面被拉到底部
+  },
+  onShareAppMessage() {
+    // 返回自定义分享信息
+    return {
+      title: 'My App',
+      desc: 'My App description',
+      path: 'pages/index/index',
+    };
+  },
+  data:{
+    // testHome:'https://746jh08117.zicp.fun/#/'
+    testHome:'http://10.129.176.234:8080/#pages/index/center?number=123ee'
+  },
+  message(e){
+    console.log(e.detail.data);
+  }
+});
